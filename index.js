cdac40be79cbd579151cd84dd872f491ce5e2c01
Oscar 1 (Usuario 1) <oscar1@oscar1.com>
2023-01-21 17:20:52 -0600

    Ruta hola y adios añadida

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const app = express();
 
 app.use(express.static(__dirname + '/public'));
 
+// Git add --patch --Este sirve para solamente subir pequeños fragmentos de código para que se puedan terminar después
+app.get('/hola', (req,res)=>{
+    res.send('Hola Ruta, estoy en la carpeta de hola')
+})
+
+
+app.get('/adios', (req,res)=>{
+    res.send('Adios Ruta, estoy en la carpeta de adios')
+})
+
+
 app.listen(3000, () => {
     console.log('Example App listening to port 3000!');
 
