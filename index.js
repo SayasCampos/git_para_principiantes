8b63be43286fec48bbe1e091a8d2779fed92218a
Oscar 1 (Usuario 1) <oscar1@oscar1.com>
2023-01-21 17:44:17 -0600

    Nuevo login: ruta, archivo HTML y estilos css

--- index.js
+++ index.js
@@ -6,13 +6,16 @@ app.use(express.static(__dirname + '/public'));
 // Git add --patch --Este sirve para solamente subir pequeños fragmentos de código para que se puedan terminar después
 app.get('/hola', (req,res)=>{
     res.send('Hola Ruta, estoy en la carpeta de hola')
-})
+});
 
 
 app.get('/adios', (req,res)=>{
     res.send('Adios Ruta, estoy en la carpeta de adios')
-})
+});
 
+app.get('/login', (req,res)=>{
+    res.send('Ruta de Login Nueva')
+});
 
 app.listen(3000, () => {
     console.log('Example App listening to port 3000!');
